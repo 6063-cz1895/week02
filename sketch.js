a4e187d1f5d2412676c9c8fb82e9bb22b03c5b45
Chenwei Zhu <cz1895@nyu.edu>
2023-09-13 15:23:59 -0400

    added two ellipses and one square

--- sketch.js
+++ sketch.js
@@ -1,5 +1,14 @@
 function setup() {
+    background(255, 0, 255);
 }
 
 function draw() {
+    stroke(0);
+    fill("orange");
+    ellipse(200, 300, 100, 100);
+    fill(0, 255, 0)
+    ellipse(300, 300, 200, 200);
+    stroke(255, 0, 0);
+    strokeweight(4);
+    rect(300, 300, 200, 200);
 }
